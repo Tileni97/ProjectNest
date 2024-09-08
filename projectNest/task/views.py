52a9f0b20c64b8b2014d459f842deeaec013b8dc
Christofine Hango <216049601@students.nust.na>
2024-09-08 12:11:12 +0200

    Task app completed

--- projectNest/task/views.py
+++ projectNest/task/views.py
@@ -20,3 +20,47 @@ def add(request, project_id, todolist_id):
         return redirect(f'/projects/{project_id}/{todolist_id}/')
 
     return render(request, 'task/add.html')
+
+
+@login_required
+def detail(request, project_id, todolist_id, pk):
+    project = Project.objects.filter(created_by=request.user).get(pk=project_id)
+    todolist = Todolist.objects.filter(project=project).get(pk=todolist_id)
+    task = Task.objects.filter(project=project).filter(todolist=todolist).get(pk=pk)
+
+    if request.GET.get('is_done', '') == 'yes':
+        task.is_done = True
+        task.save()
+
+    return render(request, 'task/detail.html', {
+        'task': task
+    })
+
+@login_required
+def edit(request, project_id, todolist_id, pk):
+    project = Project.objects.filter(created_by=request.user).get(pk=project_id)
+    todolist = Todolist.objects.filter(project=project).get(pk=todolist_id)
+    task = Task.objects.filter(project=project).filter(todolist=todolist).get(pk=pk)
+
+    if request.method == 'POST':
+        name = request.POST.get('name', '')
+        description = request.POST.get('description', '')
+
+        if name:
+            task.name = name
+
+        task.description = description
+        task.save()
+
+        return redirect(f'/projects/{project_id}/{todolist_id}/{pk}/')
+
+    return render(request, 'task/edit.html', {
+        'task': task
+    })
+
+@login_required
+def delete(request, project_id, todolist_id, pk):
+    project = Project.objects.filter(created_by=request.user).get(pk=project_id)
+    todolist = Todolist.objects.filter(project=project).get(pk=todolist_id)
+    task = Task.objects.filter(project=project).filter(todolist=todolist).get(pk=pk)
+    task.delete()
