1cb183e8e63c6b473f1a1c8b3d7ab96dcf97af05
Christofine Hango <216049601@students.nust.na>
2024-09-08 12:49:36 +0200

    Note added to projet app

--- projectNest/project/urls.py
+++ projectNest/project/urls.py
@@ -10,4 +10,11 @@ urlpatterns = [
     path('<uuid:pk>/', views.project, name='project'),
     path('<uuid:pk>/edit/', views.edit_project, name='edit'),
     path('<uuid:pk>/delete/', views.delete_project, name='delete'),
+    path('<uuid:project_id>/files/upload/', views.upload_file, name='upload_file'),
+    path('<uuid:project_id>/files/<uuid:pk>/delete/', views.delete_file, name='delete_file'),
+    path('<uuid:project_id>/notes/add/', views.add_note, name='add_note'),
+    path('<uuid:project_id>/notes/<uuid:pk>/', views.note_detail, name='note_detail'),
+    path('<uuid:project_id>/notes/<uuid:pk>/edit/', views.note_edit, name='note_edit'),
+    path('<uuid:project_id>/notes/<uuid:pk>/delete/', views.note_delete, name='note_delete'),
 ]
+
